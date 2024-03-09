e4244251d9ee8a3975d9b4f07c34727aa0248820
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-08 20:48:45 -0600

    feat: :tada: initialize git repo and project
    Initialize Spring Boot Java project with Spring initializer

---
+++ src/test/java/com/thisisbramiller/deltadentalghasassigmnet/DeltadentalghasassigmnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thisisbramiller.deltadentalghasassigmnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltadentalghasassigmnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
