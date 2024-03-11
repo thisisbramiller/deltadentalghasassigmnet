076b790ed5b1ef434abd765e3aca3bb477ad2062
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-11 08:46:56 -0500

    fix: matrix

--- backend/src/main/java/com/thisisbramiller/sorriso/controller/PolicyHolderController.java
+++ backend/src/main/java/com/thisisbramiller/sorriso/controller/PolicyHolderController.java
@@ -53,5 +53,5 @@ public class PolicyHolderController {
         policyHolderRepository.deleteById(id);
     }
 
-    
+
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,6 @@ function App() {
       <PolicyHolderCrud load={load} policyHolders={policyHolders} />
     </div>
   );
-}   
-
+}  
 
 export default App;
