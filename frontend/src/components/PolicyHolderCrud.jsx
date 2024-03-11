34a9c1c82943ab8b1c459ad11f53b3562daea4fe
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-11 00:15:27 -0500

    feat: :sparkles: add save policyholder functionality

---
+++ frontend/src/components/PolicyHolderCrud.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import api from '../api/axiosConfig';
+import PolicyHolderList from './PolicyHolderList';
+
+const PolicyHolderCrud = ({ load, policyHolders }) => {
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [policyNumber, setPolicyNumber] = useState("");
+
+    async function save(event) {
+        event.preventDefault();
+
+        await api.post("/create", {
+            name: name,
+            email: email,
+            policyNumber: policyNumber
+        });
+
+        alert("Policy Holder created successfully!");
+
+        setId("");
+        setName("");
+        setEmail("");
+        setPolicyNumber("");
+        
+        load();
+    }
+    
+}
