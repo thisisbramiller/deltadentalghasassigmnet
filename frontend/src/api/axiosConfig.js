0256df21ce0886474ac058f649ef656de8592e23
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-11 03:02:47 -0500

    refactor: :wrench: update baseurl

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://sorriso-backend-staging-7c0729e676da.herokuapp.com/'
+    baseURL: 'https://sorriso-backend-staging-7c0729e676da.herokuapp.com'
 });
